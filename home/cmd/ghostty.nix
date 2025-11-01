c609a95672a7165e1341ab10e722c10f4edbc368
Maxim Avramenko <BombasterMax@yandex.ru>
2025-11-02 01:19:47 +0300

    change default terminal to ghostty

--- home/cmd/ghostty.nix
+++ home/cmd/ghostty.nix
@@ -1,8 +1,7 @@
 { ... }:
 {
   home.file.".config/ghostty/config".text = ''
-
     font-size = 16
-    theme = gruvbox-dark
+    theme = GruvboxDark
   '';
 }
--- home/gnome/default.nix
+++ home/gnome/default.nix
@@ -77,7 +77,7 @@
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Super>Return";
       name = "Terminal";
-      command = "alacritty";
+      command = "ghostty";
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
